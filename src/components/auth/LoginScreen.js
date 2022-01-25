72c09babf33ed32ebad4965db3d4b4d6848f2eec
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-01-25 17:06:41 -0300

    LoginScreen.js and RegisterScreen: SCSS added

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,7 +1,51 @@
+import { Link } from 'react-router-dom';
+
 export const LoginScreen = () => {
   return (
     <div>
-      <h1>LoginScreen</h1>
+      <h3 className='auth__title'>Login</h3>
+
+      <form>
+        <input
+          type='text'
+          placeholder='Email'
+          name='email'
+          className='auth__input'
+          autocomplete='off'
+        />
+
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          className='auth__input mt-5'
+        />
+
+        <button type='submit' className='btn btn-primary mt-5 btn-block'>
+          Login
+        </button>
+
+        <div className='auth__social-network'>
+          <p className='mb-1'>Login with social networks</p>
+
+          <div className='google-btn '>
+            <div className='google-icon-wrapper'>
+              <img
+                className='google-icon'
+                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+                alt='google button'
+              />
+            </div>
+
+            <p className='btn-text'>
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to='/auth/register' className='link mt-5'>
+          Create a new account
+        </Link>
+      </form>
     </div>
   );
 };
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,7 +1,49 @@
+import { Link } from 'react-router-dom';
+
 export const RegisterScreen = () => {
   return (
     <div>
-      <h1>RegisterScreen</h1>
+      <h3 className='auth__title'>Register</h3>
+
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          className='auth__input mb-5'
+          autocomplete='off'
+        />
+
+        <input
+          type='text'
+          placeholder='Email'
+          name='email'
+          className='auth__input mb-5'
+          autocomplete='off'
+        />
+
+        <input
+          type='password'
+          placeholder='Password'
+          name='password'
+          className='auth__input mb-5 mb-5'
+        />
+
+        <input
+          type='password'
+          placeholder='Confirm password'
+          name='password2'
+          className='auth__input mb-5 mb-5'
+        />
+
+        <button type='submit' className='btn btn-primary mb-5 btn-block'>
+          Register
+        </button>
+
+        <Link to='/auth/login' className='link'>
+          Already Registered?
+        </Link>
+      </form>
     </div>
   );
 };
