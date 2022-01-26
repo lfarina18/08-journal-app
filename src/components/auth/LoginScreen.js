97b62043fb0fda7c369c17e77f6906a59f2b055d
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-01-26 13:22:36 -0300

    Redux, Firebase, Thunk config

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,17 +1,40 @@
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {startLoginEmailPassword } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
+
+
 
 export const LoginScreen = () => {
+
+  const dispatch = useDispatch();
+
+
+  const [formValues, handleInputChange, reset] = useForm({
+    email: 'nando@gmail.com',
+    password: '123456'
+  })
+
+  const {email, password} = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(startLoginEmailPassword(email, password));
+  }
+
   return (
     <div>
       <h3 className='auth__title'>Login</h3>
 
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type='text'
           placeholder='Email'
           name='email'
           className='auth__input'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
 
         <input
@@ -19,6 +42,9 @@ export const LoginScreen = () => {
           placeholder='Password'
           name='password'
           className='auth__input mt-5'
+          autoComplete='off'
+          value={password}
+          onChange={handleInputChange}
         />
 
         <button type='submit' className='btn btn-primary mt-5 btn-block'>
