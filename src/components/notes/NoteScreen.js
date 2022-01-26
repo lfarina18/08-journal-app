45ac4912c19b058b9b76c7354c7f6b830d25bd31
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-01-26 01:30:07 -0300

    finished user interface

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -3,8 +3,23 @@ import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
   return (
-      <div className="notes__main-content">
-          <NotesAppBar />
+    <div className='notes__main-content'>
+      <NotesAppBar />
+      <div className='notes__content'>
+        <input
+          type='text'
+          placeholder='Some awasome title'
+          className='note__title-input'
+        />
+
+        <textarea
+          placeholder='What happened today?'
+          className='notes_-textarea'></textarea>
+
+        <div className='note__image'>
+          <input type="image" src={'https://images8.alphacoders.com/490/thumb-1920-490410.jpg'} alt="car photo" />
+        </div>
       </div>
-  )
+    </div>
+  );
 };
